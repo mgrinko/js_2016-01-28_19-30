b2826b42e6a5d8bc656e643dae6a35e8230a78df
mgrinko <glukym@gmail.com>
2016-03-03 19:06:35 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let data = {
+  "Рыбы": {
+    "Форель": {},
+    "Щука": {}
+  },
+
+  "Деревья": {
+    "Хвойные": {
+      "Лиственница": {},
+      "Ель": {}
+    },
+    "Цветковые": {
+      "Берёза": {},
+      "Тополь": {}
+    }
+  }
+};
+
+let treeContainer = document.getElementById('container');
+
+new Tree({
+  element: treeContainer,
+  data: data
+});
