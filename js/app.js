89e76bebbd622c3d3fb1f94171168abc84fc73ad
mgrinko <glukym@gmail.com>
2016-03-10 20:13:08 +0200

    Lesson 2016-03-10 save

--- js/app.js
+++ js/app.js
@@ -1,26 +1,47 @@
 'use strict';
 
-let data = {
-  "Рыбы": {
-    "Форель": {},
-    "Щука": {}
+
+let images = [
+  {
+    thumbUrl: 'https://js.cx/gallery/img1-thumb.jpg',
+    url: 'https://js.cx/gallery/img1-lg.jpg',
+    title: 'Image 1'
   },
-
-  "Деревья": {
-    "Хвойные": {
-      "Лиственница": {},
-      "Ель": {}
-    },
-    "Цветковые": {
-      "Берёза": {},
-      "Тополь": {}
-    }
+  {
+    thumbUrl: 'https://js.cx/gallery/img2-thumb.jpg',
+    url: 'https://js.cx/gallery/img2-lg.jpg',
+    title: 'Image 2'
+  },
+  {
+    thumbUrl: 'https://js.cx/gallery/img3-thumb.jpg',
+    url: 'https://js.cx/gallery/img3-lg.jpg',
+    title: 'Image 3'
+  },
+  {
+    thumbUrl: 'https://js.cx/gallery/img4-thumb.jpg',
+    url: 'https://js.cx/gallery/img4-lg.jpg',
+    title: 'Image 4'
+  },
+  {
+    thumbUrl: 'https://js.cx/gallery/img5-thumb.jpg',
+    url: 'https://js.cx/gallery/img5-lg.jpg',
+    title: 'Image 5'
+  },
+  {
+    thumbUrl: 'https://js.cx/gallery/img6-thumb.jpg',
+    url: 'https://js.cx/gallery/img6-lg.jpg',
+    title: 'Image 6'
   }
-};
+  
+];
 
-let treeContainer = document.getElementById('container');
+[].forEach.call(document.querySelectorAll('[data-component="gallery"]'), function(galleryEl) {
+  let gallery = new Gallery({
+    element: galleryEl,
+    images: images
+  });
 
-new Tree({
-  element: treeContainer,
-  data: data
+  gallery.showImage(images[0].url, images[0].title);
 });
+
+
